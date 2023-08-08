e5626be9c62ba00da8c8b83739c50fe703cf8c0d
AzerAfram <97570339+AzerAfram@users.noreply.github.com>
2023-08-07 22:35:19 -0700

    Update contactUs.js

--- contactUs.js
+++ contactUs.js
@@ -1,28 +1,4 @@
 // Navigation bar states
-/*window.onscroll = function() {scrollFunction()}
-
-
-function scrollFunction() {
-
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("logo").style.width = "8.5%";
-        document.getElementById("logo").style.height = "150px";
-        document.getElementById("logo").style.padding = "50px";
-        document.getElementById("logo").style.marginTop = "-50px";
-        document.getElementById("inner_main_menu1").style.marginTop = "-107px";
-        document.getElementById("logo").style.marginLeft = "818px";
-    }
-    else {
-        document.getElementById("logo").style.width = "17%";
-        document.getElementById("logo").style.marginTop = "0";
-        document.getElementById("logo").style.padding= "0";
-        document.getElementById("logo").style.height = "300px";
-        document.getElementById("inner_main_menu1").style.marginTop = "-155px";
-        document.getElementById("logo").style.marginLeft = "791px"
-
-    }
-}
-*/
 
 let logo = document.getElementById("logo");
 window.onscroll = function() {scrollFunction()}
